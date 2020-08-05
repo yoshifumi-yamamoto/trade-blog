88020db99b1981ebea54c99fb81c10ab465cf31e
yoshifumi-yamamoto <toyotama.arank.acekiller@gmail.com>
2020-08-05 18:26:16 +0900

    [update]フッターのロゴを変更

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import logo from '../img/logo.svg'
+import nurseIcon from '../img/nurseIcon.png'
+
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
@@ -13,7 +15,7 @@ const Footer = class extends React.Component {
       <footer className="footer has-background-black has-text-white-ter">
         <div className="content has-text-centered">
           <img
-            src={logo}
+            src={nurseIcon}
             alt="Kaldi"
             style={{ width: '14em', height: '10em' }}
           />
