b203bbbb497882ed8f9ac2291b63791ccb1ff35b
yoshifumi-yamamoto <toyotama.arank.acekiller@gmail.com>
2020-08-20 10:15:33 +0900

    [add&fix]8/20相場分析を作成。getImg.pyで画像名が合わないバグを修正。

--- getImg.py
+++ getImg.py
@@ -6,18 +6,25 @@ import tinify
 tinify.key = "ZpFdqyBgrBm7Pv1d0TL8v4Q1QZ2qbb91"
 d_today = str(datetime.date.today())
 
-with open("data.html") as f:
-    html = f.read()
-    soup = BeautifulSoup(html, "html.parser") # HTMLを解析する
-    images = soup.findAll('img') # 全ての画像を取得する
-    counter = 0
-    d = {}
+try:
+    with open("data.html") as f:
+        html = f.read()
+        soup = BeautifulSoup(html, "html.parser") # HTMLを解析する
+        images = soup.findAll('img') # 全ての画像を取得する
+        counter = 0
+        d = {}
 
-    for image in images:
-        if counter != 0 and counter <= 12:
-            imgFileName = image['src'].replace("-preview","") # replaceで大きい方の画像に変換する
-            dst_path = './static/img/' + d_today + '-' + str(imgFileName).split('/')[-1] # 出力先のpathとファイル名を設定する
-            source = tinify.from_url(imgFileName) # 画像をtinypngにアップロード
-            source.to_file(dst_path) # 圧縮された画像を出力
-        counter += 1
+        for image in images:
+            if counter != 0 and counter <= 12:
+                imgFileName = image['src'].replace("-preview","") # replaceで大きい方の画像に変換する
+                if imgFileName in 'limited-2'
+                    imgFileName.replace('limited-2','limited')
+                dst_path = './static/img/' + d_today + '-' + str(imgFileName).split('/')[-1] # 出力先のpathとファイル名を設定する
+                source = tinify.from_url(imgFileName) # 画像をtinypngにアップロード
+                source.to_file(dst_path) # 圧縮された画像を出力
+            counter += 1
 
+        print("画像の取得が完了しました。")
+
+except:
+    print('画像の取得に失敗しました。')
